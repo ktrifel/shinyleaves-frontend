ff2469b7b342205d09c245a0026ae6d4d8dd54f5
annebauer04 <anne.bauer@accenture.com>
2025-06-03 14:25:35 +0200

    testtest600

--- test-frontend/src/app/app.routes.ts
+++ test-frontend/src/app/app.routes.ts
@@ -5,14 +5,36 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { ImpressumComponent } from './pages/impressum/impressum.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'products', pathMatch: 'full' },
-  { path: 'products', component: ProductListComponent },
-  { path: 'cart', component: CartComponent },
-  { path: 'checkout', component: CheckoutComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'impressum', component: ImpressumComponent },
-  { path: '**', redirectTo: 'products' }
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { 
+    path: 'products', 
+    loadComponent: () => import('./components/product-list/product-list.component')
+      .then(m => m.ProductListComponent) 
+  },
+  { 
+    path: 'cart', 
+    loadComponent: () => import('./components/cart/cart.component')
+      .then(m => m.CartComponent) 
+  },
+  { 
+    path: 'login', 
+    loadComponent: () => import('./pages/login/login.component')
+      .then(m => m.LoginComponent) 
+  },
+  { 
+    path: 'register', 
+    loadComponent: () => import('./pages/register/register.component')
+      .then(m => m.RegisterComponent) 
+  },
+
+  {
+    path: 'impressum',
+    loadComponent: () => import('./pages/impressum/impressum.component')
+      .then(m => m.ImpressumComponent)
+  },
+
+  
+  { path: '**', redirectTo: '/products' }
 ];
 
 export const appConfig: ApplicationConfig = {
