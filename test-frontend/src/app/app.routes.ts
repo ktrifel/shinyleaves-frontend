9d5815beea5f6ded1efefebf4038dac65a78f09f
Asteroid <d.engel@schueler.bs1-bt.de>
2025-06-03 08:59:15 +0200

    Log In Interface

--- test-frontend/src/app/app.routes.ts
+++ test-frontend/src/app/app.routes.ts
@@ -16,6 +16,9 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '**', redirectTo: 'products' }
+
+
+
 ];
 
 export const appConfig: ApplicationConfig = {
