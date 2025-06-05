551537e4f28d1d601f685595d44fee06f2fcef7b
NBalleBS1 <144804295+NBalleBS1@users.noreply.github.com>
2025-06-05 11:23:39 +0200

    test4

--- test-frontend/src/app/app.config.ts
+++ test-frontend/src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes as appRoutes } from './app.routes';
+import { jwtInterceptor } from './core/jwt.interceptor';
 
 
 export const routes = [
@@ -12,6 +13,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes),
     provideAnimations(),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([jwtInterceptor]))
   ]
 };
